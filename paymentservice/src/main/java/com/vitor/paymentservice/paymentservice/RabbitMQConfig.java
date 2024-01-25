2361bf3080dea9cd6cf40b349d04e1e3432a27d2
Vitor <vitorvitinho9900@hotmail.com>
2024-01-24 21:23:51 -0300

    Começo parte 2

---
+++ paymentservice/src/main/java/com/vitor/paymentservice/paymentservice/RabbitMQConfig.java
@@ -0,0 +1,28 @@
+package com.vitor.paymentservice.paymentservice;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queue() {
+        return new Queue("yourQueueName", true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("yourExchangeName");
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("yourRoutingKey");
+    }
+}
+
---
+++ reservationservice/src/main/java/com/vitor/reservationservice/reservationservice/RabbitMQConfig.java
@@ -0,0 +1,29 @@
+package com.vitor.reservationservice.reservationservice;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public Queue queue() {
+        return new Queue("yourQueueName", true);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange("yourExchangeName");
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("yourRoutingKey");
+    }
+
+    // ... outras configurações necessárias ...
+}
