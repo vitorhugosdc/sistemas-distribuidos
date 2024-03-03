451433b13e454314ed95dd8a4f65c609bc24dabd
Vitor <vitorvitinho9900@hotmail.com>
2024-03-03 10:45:03 -0300

    reservas sem duplicação

--- roomservice/src/main/java/com/vitor/roomservice/roomservice/service/RoomService.java
+++ roomservice/src/main/java/com/vitor/roomservice/roomservice/service/RoomService.java
@@ -23,9 +23,8 @@ public class RoomService {
     }
 
     public String makeReservation(String clientName, String roomNumber, String paymentMethod) {
-        String availabilityResult = checkRoomAvailability(roomNumber);
-        if (!availabilityResult.contains("available")) {
-            return availabilityResult; // Room not available
+        if (reservations.containsKey(roomNumber)) {
+            return "Room service: Room " + roomNumber + " is not available for reservation.";
         }
 
         // Preparar a mensagem para o serviço de pagamento
@@ -38,6 +37,11 @@ public class RoomService {
         rabbitTemplate.convertAndSend("payments-exchange", "process.payment", paymentInfo);
 
         // Retorno imediato, assumindo que o processamento será assíncrono
+        addReservation(roomNumber); // Adiciona a reserva após verificar a disponibilidade
         return "Payment processing initiated. Please wait for confirmation.";
+    }
+
+    private void addReservation(String roomNumber) {
+        reservations.put(roomNumber, roomNumber);
     }
 }
