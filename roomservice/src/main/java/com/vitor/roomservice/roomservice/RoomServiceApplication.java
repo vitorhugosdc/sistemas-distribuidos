edda2fe37bee4b9ced8aae513bb07227a5341a6f
Vitor <vitorvitinho9900@hotmail.com>
2024-03-24 00:41:22 -0300

    T3 finalizado

--- roomservice/src/main/java/com/vitor/roomservice/roomservice/RoomServiceApplication.java
+++ roomservice/src/main/java/com/vitor/roomservice/roomservice/RoomServiceApplication.java
@@ -2,9 +2,11 @@ package com.vitor.roomservice.roomservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @Import(RestTemplateConfig.class)
 public class RoomServiceApplication {
 
